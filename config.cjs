e628af468c6aa00e0532bbfdf4439b25c7fb5699
Rush <dilshanrashmika552@gmail.com>
2024-07-07 12:00:27 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,7 +19,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "Rushhide",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94761111111",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94761111112",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
